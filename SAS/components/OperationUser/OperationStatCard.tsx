2a35506f3442476549cb04b706f4b614fda3eea8
Siyumi Gamage <siyumisandeepani2002@gmail.com>
2026-04-22 19:51:26 +0530

    new edit navbar

--- SAS/components/OperationUser/OperationStatCard.tsx
+++ SAS/components/OperationUser/OperationStatCard.tsx
@@ -3,8 +3,6 @@ import '@/styles/OperationStyles/OperationStatCard.css';
 type Props = {
   title: string;
   value: string;
-  //meta: string;
-  //tone: 'blue' | 'purple' | 'green' | 'red';
   icon: React.ReactNode;
 };
 
@@ -12,7 +10,6 @@ export default function OperationStatCard({ title, value, icon }: Props) {
   return (
     <div className="op-stat">
       <div className="op-stat__top">
-        {/* <div className={`op-stat__dot op-stat__dot--${tone}`} */}
         <span className="op-stat__title">{title}</span>
 
         <div className="op-stat__iconWrap">{icon}</div>
@@ -20,7 +17,6 @@ export default function OperationStatCard({ title, value, icon }: Props) {
 
       <div className="op-stat__valueRow">
         <div className="op-stat__value">{value}</div>
-        {/*<div className={`op-stat__meta op-stat__meta--${tone}`}>{meta}</div>*/}
       </div>
     </div>
   );
