4ce46f6b4f82dc433e75f3b47c0b5eafc9d6d72f
ronakasampath <samaraweerasdrs.23@uom.lk>
2026-04-24 15:07:23 +0530

    feat: add operation and sales user dashboard and layout updates

--- SAS/app/(protected)/operation_user/layout.tsx
+++ SAS/app/(protected)/operation_user/layout.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
-import AdminLeftNavBar from '@/components/AdminUser/AdminLeftNavBar';
+import AdminLeftNavBar from '@/components/OperationUser/OperationLeftNavBar';
 import AdminTopBar from '@/components/AdminUser/AdminTopBar';
+import OperationLeftNavBar from '@/components/OperationUser/OperationLeftNavBar';
 
 export default function AdminLayout({ children }: { children: React.ReactNode }) {
   return (
     <div style={{ display: 'flex', minHeight: '100vh' }}>
-      <AdminLeftNavBar />
+      <OperationLeftNavBar/>
       <div style={{ marginLeft: '260px', flex: 1, display: 'flex', flexDirection: 'column', minHeight: '100vh' }}>
         <AdminTopBar />
         <main style={{ flex: 1, background: '#f9fafb', padding: '24px' }}>
--- SAS/app/(protected)/sales_user/layout.tsx
+++ SAS/app/(protected)/sales_user/layout.tsx
@@ -1,11 +1,11 @@
 import React from 'react';
-import SuperLeftNavBar from '@/components/SuperUser/SuperLeftNavBar';
 import AdminTopBar from '@/components/AdminUser/AdminTopBar';
+import SalesLeftNavBar from '@/components/SalesUser/SalesLeftNavBar';
 
 export default function OperationLayout({ children }: { children: React.ReactNode }) {
   return (
     <div style={{ display: 'flex', minHeight: '100vh' }}>
-      <SuperLeftNavBar/>
+      <SalesLeftNavBar/>
       <div style={{ marginLeft: '260px', flex: 1, display: 'flex', flexDirection: 'column', minHeight: '100vh' }}>
         <AdminTopBar/>
         <main style={{ flex: 1, background: '#f9fafb', padding: '24px' }}>
