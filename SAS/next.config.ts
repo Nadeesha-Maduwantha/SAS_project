5fe634558631dd925db5586e570f7c6ccfe12297
nadeesha maduwantha sugathadasa <sugathadasapdnm.23@uom.lk>
2026-07-14 11:25:01 +0530

    fix(next.config): update allowedDevOrigins to correct IP address

--- SAS/next.config.ts
+++ SAS/next.config.ts
@@ -4,7 +4,7 @@ const nextConfig: NextConfig = {
   turbopack: {
     root: __dirname,
   },
-  allowedDevOrigins: ['10.16.245.119'],
+  allowedDevOrigins: ['10.189.227.119'],
 }
 
 export default nextConfig
